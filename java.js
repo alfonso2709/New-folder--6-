63fe645a5d616e7d30cf5a7aa2f4ee3bc72a093e
Alfonso <127aramirez@frhsd.com>
2025-12-15 14:02:56 -0500

    git

---
+++ java.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById("btn")
+btn.addEventListener("click", getWords)
+function getWords(){
+    console.log(btn)
+}
+
+let words = document.querySelectorAll("input[type=text]")
+    console.log(words)
+
