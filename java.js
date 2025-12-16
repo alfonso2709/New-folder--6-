1e7d4986a3b11060645b1c0e2b662d4dcff8db83
Alfonso <127aramirez@frhsd.com>
2025-12-16 14:01:59 -0500

    jhgjhkfa

--- java.js
+++ java.js
@@ -1,9 +1,19 @@
 let btn = document.getElementById("btn")
+
+
 btn.addEventListener("click", getWords)
 function getWords(){
     console.log(btn)
-}
-
-let words = document.querySelectorAll("input[type=text]")
+    
+    var words = document.querySelectorAll("input[type=text]")
     console.log(words)
+    
+    let  placeholders = document.querySelectorAll(".userInput")
+    console.log(placeholders)    
+    
+    placeholders.forEach((placeholder, index)=> {
+        console.log(placeholder, index);
+    });
 
+    console.log(67);
+}
